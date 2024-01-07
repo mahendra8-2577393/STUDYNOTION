be13d434b722cff82aded9f72693d1fa9cf15dde
Mahendra Kumar Dambiwal <ms2577393@gmail.com>
2024-01-07 19:50:06 +0530

    contact

--- src/components/core/ContactUsPage/ContactDetails.jsx
+++ src/components/core/ContactUsPage/ContactDetails.jsx
@@ -8,20 +8,20 @@ const contactDetails = [
     icon: "HiChatBubbleLeftRight",
     heading: "Chat on us",
     description: "Our friendly team is here to help.",
-    details: "info@studynotion.com",
+    details: "mahi@studynotion.com",
   },
   {
     icon: "BiWorld",
     heading: "Visit us",
     description: "Come and say hello at our office HQ.",
     details:
-      "Akshya Nagar 1st Block 1st Cross, Rammurthy nagar, Bangalore-560016",
+      "Vaishali Nagar 1st Block 1st Cross,Khora Bisal, Jaipur-302012",
   },
   {
     icon: "IoCall",
     heading: "Call us",
     description: "Mon - Fri From 8am to 5pm",
-    details: "+123 456 7869",
+    details: "+8824491970",
   },
 ]
 
